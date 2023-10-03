74549ad33ce1bae120a1b3358bc4972b49d80baf
root <root@U-AA-HXH91T3-L>
2023-10-03 13:30:26 +0100

    Added a simple multiplier

--- Program.cs
+++ Program.cs
@@ -2,7 +2,20 @@
 class Program
 {
     static void Main(string[] args)
+
     {
+        
         Console.WriteLine("Welcome to the calculator!");
+        Console.WriteLine("Write a number");
+        string? str1 = Console.ReadLine();
+        float num1 = float.Parse(str1);
+
+
+        Console.WriteLine ("what is your second number");
+        string? str2 = Console.ReadLine();
+        float num2 = float.Parse(str1);
+
+        Console.WriteLine (num2 * num1);
+
     }
 }
